c615b6a79e0647f0fb5f644be8fc892541a0c8e3
ajay-raut <ajayraut2500@gmail.com>
2026-03-27 13:30:43 +0530

    updated main and variables tf file

--- main.tf
+++ main.tf
@@ -1,47 +1,64 @@
-# Find the latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-# Create a Security Group to allow SSH
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "app_server_sg" {
+  name        = "app_server_sg"
+  description = "Allow SHH and TCP traffic"
 
   ingress {
+    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # For production, restrict this to your IP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
+    description = "Allow out bonds traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "AppServerSecurityGroup"
+  }
 }
 
-# Launch the EC2 Instance
-resource "aws_instance" "my_ec2" {
-  ami           = data.aws_ami.amazon_linux_2.id
+resource "aws_instance" "app_server" {
+  ami           = "ami-019715e0d74f695be"
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                apt-get update
+                apt-get install apache2 wget unzip vim -y
+                systemctl enabled apache2
+                mkdir -p /tmp/finance
+                cd /tmp/finance
+                wget https://www.tooplate.com/zip-templates/2135_mini_finance.zip
+                unzip -o 2135_mini_finance.zip
+                cp -r 2135_mini_finance/* /var/www/html/
+                systemctl restart apache2
+                cd /tmp/
+                rm -rf /tmp/finance            
+              EOF
 
   tags = {
     Name = var.instance_name
-    Environment = "Dev"
   }
 }
 
 # Output the Public IP after creation
 output "instance_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.app_server.public_ip
 }
